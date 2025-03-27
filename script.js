e6d0820834a5c6c7909a3472e44406cb1a457082
cha-law <char_law@outlook.com>
2025-03-27 19:25:53 +0000

    Finished form validating

--- script.js
+++ script.js
@@ -17,8 +17,29 @@ email.addEventListener("input", () => {
     displayEmailError();
 })
 
+pass.addEventListener("input", () => {
+    displayPasswordError();
+    displayConfirmPasswordError();
+})
+
+confirmPass.addEventListener("input", () => {
+    displayConfirmPasswordError();
+})
+
+country.addEventListener("input", () => {
+    displayCountryError();
+})
+
+postCode.addEventListener("input", () => {
+    displayPostCodeError();
+})
+
 function checkFormErrors() {
     displayEmailError();
+    displayPasswordError();
+    displayConfirmPasswordError();
+    displayCountryError();
+    displayPostCodeError();
 }
 
 function displayEmailError() {
@@ -28,18 +49,84 @@ function displayEmailError() {
         errorMessage.parentElement.classList = "error"; 
         errorMessage.classList = "error active";
     } else {
-        errorMessage.parentElement.classList = "passed"; 
-        errorMessage.classList = "error";
-        errorMessage.textContent = "";
+        makeValid(errorMessage);
     }
 
     if (email.validity.valueMissing) {
-        errorMessage.textContent = "! Empty Value";
+        errorMessage.textContent = "^ Empty Value";
     } else if (email.validity.typeMismatch) {
-        errorMessage.textContent = "! Needs to be an email address";
+        errorMessage.textContent = "^ Needs to be an email address";
     }
 }
 
 function displayPasswordError() {
     const errorMessage = document.querySelector(".error#error-pass");
+
+    if (!pass.validity.valid) {
+        errorMessage.parentElement.classList = "error"; 
+        errorMessage.classList = "error active";
+    } else {
+        makeValid(errorMessage);
+    }
+
+    if (pass.validity.valueMissing) {
+        errorMessage.textContent = "^ Empty Value";
+    } else if (pass.validity.tooShort) {
+        errorMessage.textContent = "^ Must be over 8 characters";
+    }
+}
+
+function displayConfirmPasswordError() {
+    const errorMessage = document.querySelector(".error#error-confirm-pass");
+    if (confirmPass.validity.valueMissing) {
+        errorMessage.parentElement.classList = "error"; 
+        errorMessage.classList = "error active";
+        errorMessage.textContent = "^ Empty value"; 
+    } else if (confirmPass.value != pass.value) {
+        errorMessage.parentElement.classList = "error"; 
+        errorMessage.classList = "error active";
+        errorMessage.textContent = "^ Must be the same as password"; 
+    } else {
+        makeValid(errorMessage);
+    }
+}
+
+function displayCountryError() {
+    const errorMessage = document.querySelector(".error#error-country");
+
+    if (!country.validity.valid) {
+        errorMessage.parentElement.classList = "error"; 
+        errorMessage.classList = "error active";
+    } else {
+        makeValid(errorMessage);
+    }
+
+    if (country.validity.valueMissing) {
+        errorMessage.textContent = "^ Empty Value";
+    } else if (country.validity.typeMismatch) {
+        errorMessage.textContent = "^ Must be characters";
+    }
+}
+
+function displayPostCodeError() {
+    const errorMessage = document.querySelector(".error#error-postcode");
+
+    if (!postCode.validity.valid) {
+        errorMessage.parentElement.classList = "error"; 
+        errorMessage.classList = "error active";
+    } else {
+        makeValid(errorMessage);
+    }
+
+    if (postCode.validity.valueMissing) {
+        errorMessage.textContent = "^ Empty Value";
+    } else if (postCode.validity.typeMismatch) {
+        errorMessage.textContent = "^ Must be characters";
+    }
+}
+
+function makeValid(errorMessage) {
+    errorMessage.parentElement.classList = "passed"; 
+    errorMessage.classList = "error";
+    errorMessage.textContent = "";
 }
